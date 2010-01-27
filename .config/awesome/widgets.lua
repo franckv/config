34f9a3447b14fc2ba173a3f0e4159ffb8c940429
franck <franck@Yggdrasil.localdomain>
2010-01-27 01:18:38 +0100

    Move widgets to external file

--- .config/awesome/widgets.lua
+++ .config/awesome/widgets.lua
@@ -1,9 +1,28 @@
+local io = io
+local os = os
+local string = string
+local table = table
+local math = math
+local tonumber = tonumber
+local awful = require("awful")
+local widget = widget
+local timer = timer
+require("lib/mpd")
+local mpd = mpd
+
+module("widgets")
+
+batterywidget = widget({ type = "textbox" })
+mpdwidget = widget({ type = "textbox" })
+mpdwidget:buttons({awful.button({ }, 1, function () mpd.toggle_play() end)})
+fswidget = widget({ type = "textbox" })
+
 function setFg(color, text)
     return '<span color="'..color..'">'..text..'</span>'
 end
 
 function batteryInfo(adapter)
-    mybatterywidget.text = adapter
+    batterywidget.text = adapter
 
     local spacer = " "
     local fcur = io.open("/sys/class/power_supply/"..adapter.."/charge_now")    
@@ -12,44 +31,48 @@ function batteryInfo(adapter)
 
     if not fcur or not fcap or not fsta then
 	dir = "="
-	battery = "A/C"
-        mybatterywidget.text = dir..battery..dir.." "
+	status = "A/C"
+        batterywidget.text = dir..status..dir.." "
         return
     end
 
     local cur = fcur:read()
     local cap = fcap:read()
     local sta = fsta:read()
-    local battery = math.floor(cur * 100 / cap)
+    local status = math.floor(cur * 100 / cap)
     if sta:match("Charging") then
 	dir = "^"
-	battery = "A/C ("..battery..")"
+	status = "A/C ("..status..")"
     elseif sta:match("Discharging") then
 	dir = "v"
-	if tonumber(battery) > 25 and tonumber(battery) < 75 then
-	    battery = setFg("#e6d51d", battery)
-	elseif tonumber(battery) < 25 then
-	    if tonumber(battery) < 10 then
+	if tonumber(status) > 25 and tonumber(status) < 75 then
+	    status = setFg("#e6d51d", status)
+	elseif tonumber(status) < 25 then
+	    if tonumber(status) < 10 then
 		naughty.notify({ title      = "Battery Warning"
-		, text       = "Battery low!"..spacer..battery.."%"..spacer.."left!"
+		, text       = "Battery low!"..spacer..status.."%"..spacer.."left!"
 		, timeout    = 5
 		, position   = "top_right"
 		, fg         = beautiful.fg_focus
 		, bg         = beautiful.bg_focus })
 	    end
-	    battery = setFg("#ff6565", battery)
+	    status = setFg("#ff6565", status)
 	else
-	    battery = battery
+	    status = status
 	end
     else
 	dir = "="
-	battery = "A/C"
+	status = "A/C"
     end
-    mybatterywidget.text = dir..battery..dir.." "
+    batterywidget.text = dir..status..dir.." "
     fcur:close()
     fcap:close()
     fsta:close()
 end
+batteryInfo("BAT0")
+batTimer = timer { timeout = 20 }
+batTimer:add_signal("timeout", function() batteryInfo("BAT0") end)
+batTimer:start()
 
 function mpdInfo()
   local stats = mpd.send("status")
@@ -66,15 +89,18 @@ function mpdInfo()
 
   local pos = stats.song + 1
   local len = stats.playlistlength
-  --local zstats = mpd.send("playlistid " .. stats.songid)
   local zstats = mpd.send("currentsong")
-  local artist = zstats.artist
-  local song = zstats.title
+  local artist = awful.util.escape(zstats.artist)
+  local song = awful.util.escape(zstats.title)
   local current = os.date("%M:%S", stats.time:match("(%d+):"))
   local total = os.date("%M:%S", stats.time:match("%d+:(%d+)"))
 
   mpdwidget.text = ' <i>' .. artist .. " - " .. song .. ' ' .. current .. "/" .. total .. ' [' .. pos .. '/' .. len .. ']</i> '
 end
+mpdInfo()
+mpdTimer = timer {timeout = 2}
+mpdTimer:add_signal("timeout", function() mpdInfo() end)
+mpdTimer:start()
 
 function splitbywhitespace(str)
     values = {}
@@ -118,5 +144,8 @@ function fsInfo(format)
     f:close()
     fswidget.text = result
 end
-
+fsInfo("[/:${/ used}/${/ size} ~:${/home used}/${/home size}] ")
+fsTimer = timer {timeout = 30}
+fsTimer:add_signal("timeout", function() fsInfo('[/:${/ used}/${/ size} ~:${/home used}/${/home size}] ') end)
+fsTimer:start()
 
