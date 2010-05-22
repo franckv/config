b24fc0104ecf55cd01b315fdc12196c18ea80d37
franck <franck.vdl@gmail.com>
2010-05-22 16:14:59 +0200

    Fix firefox rules

--- .config/awesome/rules.lua
+++ .config/awesome/rules.lua
@@ -1,5 +1,6 @@
 awful.rules.rules = awful.util.table.join(awful.rules.rules, {
     { rule = { class = "Navigator" }, properties = { tag = tags[1][2] } },
+    { rule = { class = "Namoroka" }, properties = { tag = tags[1][2] } },
     { rule = { class = "Sonata" }, properties = { tag = tags[1][3] } },
     { rule = { class = "Vlc" }, properties = { tag = tags[1][4] } },
     { rule = { class = "Pidgin" }, properties = { tag = tags[1][5] } },
